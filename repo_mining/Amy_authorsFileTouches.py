0b28b05fb14cdefa9205a26f2bd735917ffc68b9
amai83 <amyfeng1082@gmail.com>
2024-06-29 00:19:15 -0700

    adjusted again

--- repo_mining/Amy_authorsFileTouches.py
+++ repo_mining/Amy_authorsFileTouches.py
@@ -71,7 +71,7 @@ repo = 'scottyab/rootbeer'
 # Remember to empty the list when going to commit to GitHub.
 # Otherwise they will all be reverted and you will have to re-create them
 # I would advise to create more than one token for repos with heavy commits
-lstTokens = ["ghp_shjCPSicu3TEjgbCXTiqdRmIpEQAEnaw7yP6"]
+lstTokens = ["lol_shjCPSicu3TEjgbCXTiqdRmIpEQAEnaw7yP6"]
 
 dictfiles = dict()
 collect_file_touches(dictfiles, lstTokens, repo)
